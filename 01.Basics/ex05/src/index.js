61b2e999e441cf2e417445ed72d37d44e6a19304
cocoball97 <wlrn0514@gmail.com>
2025-02-07 17:13:46 +0900

    react #2

---
+++ 01.Basics/ex05/src/index.js
@@ -0,0 +1,8 @@
+import ReactDom from 'react-dom/client'
+import {App} from './app.js'
+
+// document
+//     .getElementById("root")
+//     .appendChild(App());
+const root = ReactDom.createRoot(document.getElementById('root').appendChild(App()));
+root.render(App());
